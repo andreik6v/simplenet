5496476eaa5975d80187eda26dcb44350fce7a4a
Andrei Chira <andreichira777@gmail.com>
2024-04-18 21:54:56 +0300

    new patterns

---
+++ patterns/hero-light.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Hero Light
+ * Slug: hero-light
+ * Description:
+ * Categories: henry-block/hero
+ */
+?>
+
+<!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns alignwide" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"width":"65%","gradient":"gradient-6"} -->
+<div class="wp-block-column has-gradient-6-gradient-background has-background" style="flex-basis:65%"><!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"4rem","fontStyle":"normal","fontWeight":"600"}},"className":"is-style-asterisk"} -->
+<h1 class="wp-block-heading is-style-asterisk" style="font-size:4rem;font-style:normal;font-weight:600">Effortlessly <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-secondary-color">create</mark> WordPress <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-secondary-color">websites</mark> with clicks, not code.</h1>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"className":"is-style-asterisk"} -->
+<p class="is-style-asterisk">Henry is a modern block-based WordPress theme hand-crafted by Andrei Chira, a long-time WordPress creator and Gutenberg enthusiast.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"layout":{"type":"flex"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#" target="_blank" rel="noreferrer noopener">Free Download</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":""} -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
