9dc82ab01f2ecda6a0dbe17ef799d9ad616d9f69
Andrei Chira <andreichira777@gmail.com>
2025-06-24 22:32:37 +0300

    Update hidden-post-meta.php

--- patterns/hidden-post-meta.php
+++ patterns/hidden-post-meta.php
@@ -7,8 +7,10 @@
 ?>
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0.3em"}},"layout":{"type":"flex","justifyContent":"left"}} -->
-<div class="wp-block-group"><!-- wp:post-author-name {"isLink":true} /-->
+<div class="wp-block-group">
+    <!-- wp:group {"style":{"spacing":{"blockGap":"0.3em"}},"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-group">
+    <!-- wp:post-author-name {"isLink":true} /-->
 
 <!-- wp:paragraph {"textColor":"theme-5"} -->
 <p class="has-theme-5-color has-text-color">·</p>
@@ -20,6 +22,8 @@
 <p class="has-theme-5-color has-text-color">·</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:post-terms {"term":"category"} /--></div>
-<!-- /wp:group --></div>
+<!-- wp:post-terms {"term":"category"} /-->
+</div>
+<!-- /wp:group -->
+</div>
 <!-- /wp:group -->
