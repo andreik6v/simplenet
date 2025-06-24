c544057ccf79b7a2eef27e6b9d2de011f16f1865
Andrei Chira <andreichira777@gmail.com>
2025-06-24 22:25:34 +0300

    Update hidden-post-meta.php

--- patterns/hidden-post-meta.php
+++ patterns/hidden-post-meta.php
@@ -7,24 +7,19 @@
 ?>
 
 <!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
-	<!-- wp:group {"style":{"spacing":{"blockGap":"0.3em"}},"layout":{"type":"flex","justifyContent":"left"}} -->
-	<div class="wp-block-group">
-		<!-- wp:post-date {"format":"M j, Y","isLink":true} /-->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0.3em"}},"layout":{"type":"flex","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:post-author-name {"isLink":true} /-->
 
-		<!-- wp:paragraph {"textColor":"theme-5"} -->
-		<p class="has-theme-5-color has-text-color">—</p>
-		<!-- /wp:paragraph -->
+<!-- wp:paragraph {"textColor":"theme-5"} -->
+<p class="has-theme-5-color has-text-color">·</p>
+<!-- /wp:paragraph -->
 
-		<!-- wp:paragraph {"fontSize":"small","textColor":"theme-5"} -->
-		<p class="has-small-font-size has-theme-5-color has-text-color"><?php echo esc_html_x( 'by', 'Prefix for the post author block: By author name', 'simplenet' ); ?></p>
-		<!-- /wp:paragraph -->
+<!-- wp:post-date {"format":"M j, Y"} /-->
 
-		<!-- wp:post-author-name {"isLink":true} /-->
+<!-- wp:paragraph {"textColor":"theme-5"} -->
+<p class="has-theme-5-color has-text-color">·</p>
+<!-- /wp:paragraph -->
 
-		<!-- wp:post-terms {"term":"category","prefix":"<?php echo esc_html_x( 'in ', 'Prefix for the post category block: in category name', 'simplenet' ); ?>"} /-->
-
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:post-terms {"term":"category"} /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
